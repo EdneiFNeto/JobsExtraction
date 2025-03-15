bcb471ef589d62ace99683c6670c6f926c4270f3
EdneiFNeto <edneifneto@gmail.com>
2025-03-15 20:09:36 -0300

    Find image

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,29 @@
 import puppeteer from 'puppeteer';
+import fs from 'fs';
 
 (async () => {
   // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
+  
+  // Abre uma nova página
   const page = await browser.newPage();
+  
+  // Navega até o site desejado
+  await page.goto('https://fatalmodel.com/acompanhantes-rio-de-janeiro-rj');
+  
+  const list = await page.evaluate(() => {
+    const nodeList = document.querySelectorAll('section img');
+    const array = [...nodeList];
+    return array.map(element => { return element.src });
+  });
 
-  // Navigate the page to a URL
-  await page.goto('https://www.mercadolivre.com.br/');
 
-  // Set screen size
-  await page.screenshot("screenshot.png", { fullPage: true });
+  fs.writeFile('data.json', JSON.stringify(list, null, 2), err => {
+    if (err) throw new Error('something went wrong');
+  });
 
+  
+  // Fecha o navegador
   await browser.close();
 })();
 
